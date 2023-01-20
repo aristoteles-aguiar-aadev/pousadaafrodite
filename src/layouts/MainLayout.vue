314ea5f4f962f1c62321868dfd42460c9996d407
aristoteles-aguiar-aadev <aristotelesouza@gmail.com>
2023-01-20 01:13:52 -0300

    pousada afrodite teste

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-grey-3 text-grey-9">
+      <q-toolbar style="background:#EEEEEE ">
+
+
+        <q-toolbar-title style="margin:20px" class="text-center">
+          <div class="text-h2" style="font-family: 'Poppins', sans-serif; color:orange">Pousada Afrodite</div>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="drawer" show-if-above width="220" bordered breakpoint-up="30" class="bg-grey-3 text-grey-7"
+      width:200px>
+      <img style="width: 120px; margin: 10px 0 0 50px;" src="../assets/img/pousada-afrodite-2.png" alt="" srcset="">
+
+
+      <q-list>
+        <q-item-label header>
+
+          <div style="margin-left:55px">Bem Vindo!</div>
+
+          <q-separator class="q-mb-md" />
+        </q-item-label>
+        <q-space></q-space>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+import { list } from 'postcss'
+
+const linksList = [
+  {
+    title: 'Home',
+    caption: 'inicio',
+    link: 'https://pousadaafrodite.com.br'
+  },
+  {
+    title: 'Acomodações',
+    caption: 'Ambiente',
+    link: 'https://pousadaafrodite.com.br/pousada'
+  },
+  {
+    title: 'Serviços',
+    caption: 'Bar / Loja',
+    link: 'https://pousadaafrodite.com.br/pousada'
+  },
+
+
+
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-center" style="padding: 5%;">
+    <q-card style="width:800px; height:auto">
+      <q-card-section>
+        <div class="text-h2" style="font-family: 'Tourney', cursive; color:green; text-align: end;">
+          O naturalismo</div>
+        <q-separator class="q-mb-md" />
+        <div class="text-subtitle" style="font-size: 20px; text-align:end; color: grey; margin-bottom: 150px;">bem
+          perto de você, vem
+          se
+          divertir !
+        </div>
+
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+
+          <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+          <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+          <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+          <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+          <q-toggle v-model="accept" label="I accept the license and terms" />
+
+          <div>
+            <q-btn label="Gravar" type="submit" color="primary" />
+            <q-btn label="Limpar" type="reset" color="warning" flat class="q-ml-sm" />
+          </div>
+        </q-form>
+      </q-card-section>
+
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
